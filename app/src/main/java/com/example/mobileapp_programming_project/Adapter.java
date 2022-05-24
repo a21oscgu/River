6e5768b8b625e08cf43fa46865a4ec8506361509
a21oscgu <a21oscgu@student.his.se>
2022-05-24 21:39:10 +0200

    Added an adapter

---
+++ app/src/main/java/com/example/mobileapp_programming_project/Adapter.java
@@ -0,0 +1,35 @@
+package com.example.mobileapp_programming_project;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class Adapter extends RecyclerView.Adapter<ViewHolder> {
+
+    ArrayList<River> rivers;
+    public Adapter(ArrayList<River> rivers) {
+        this.rivers = rivers;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View my_view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new ViewHolder(my_view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.item.setText(rivers.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return rivers.size();
+    }
+}
